ee6612b3751e8ae713a18e7c991de68863ec8fb1
devincalmt <devincalimto@gmail.com>
2020-02-21 08:34:16 +0700

    nav

--- src/javascript/Header.js
+++ src/javascript/Header.js
@@ -4,7 +4,18 @@ import '../styling/Header.scss'
 function Header(){
     return(
         <div>
-            Devinca Limto
+            <div className="header-container split">
+                <div>Dev</div>
+                <div className="nav-bar horizontal">
+                    <div className="nav-item">Home</div>
+                    <div className="nav-item">About Me</div>
+                    <div className="nav-item">Education</div>
+                    <div className="nav-item">Skills</div>
+                    <div className="nav-item">Works</div>
+                    <div className="nav-item">Achievements</div>
+                    <div className="nav-item">Contact</div>
+                </div>
+            </div>
         </div>
     );
 }
