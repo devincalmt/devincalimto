dd3cd95e002165de0f9e32ad6aa52dc1d961c293
devincalmt <devincalimto@gmail.com>
2022-04-11 08:37:52 +0700

    contacts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pink-pastel-100': '#ffeaea',
+        'pink-pastel-200': '#ffe1e1',
+        'pink-pastel-300': '#ffd8d8',
+        'pink-pastel-400': '#ffc3c3',
+        'pink-pastel-500': '#ffb0b0'
+      }
+    },
   },
   plugins: [],
 }
